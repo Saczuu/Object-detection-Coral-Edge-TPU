da56a7bfefe2d32023d706cb8cbf54f8cacc7855
root <saczewski.maciej@outlook.com>
2019-06-25 11:08:15 +0000

    first try of retraingin 50 eteps & 10 eval steps

--- retrain.sh
+++ retrain.sh
@@ -1,7 +1,7 @@
 export PYTHONPATH=/Users/maciejsaczewski/Documents/Inzynier/Object_detection_Coral/slim:$PYTHONPATH
 
-NUM_TRAINING_STEPS=1 && \
-NUM_EVAL_STEPS=1
+NUM_TRAINING_STEPS=50 && \
+NUM_EVAL_STEPS=10
 
 ./scripts/retrain_detection_model.sh \
 --num_training_steps ${NUM_TRAINING_STEPS} \
