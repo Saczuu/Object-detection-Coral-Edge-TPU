0464c30d48e2d018c696771634c3ebc9c015ba44
saczewski <saczewski.maciej@outlook.com>
2019-07-23 22:27:28 +0200

    Predicitons scripts

--- predict.py
+++ predict.py
@@ -11,17 +11,18 @@ import cv2
 
 # Function to read labels from text files.
 def ReadLabelFile(file_path):
-    with open(file_path, 'r', encoding="utf-8") as f:
-        lines = f.readlines()
-    ret = {}
-    for line in lines:
-        pair = line.strip().split(maxsplit=1)
-        ret[int(pair[0])] = pair[1].strip()
-    return ret
+	with open(file_path, 'r', encoding="utf-8") as f:
+		lines = f.readlines()
+	ret = {}
+	for line in lines:
+		pair = line.strip().split(maxsplit=1)
+		ret[int(pair[0])] = pair[1].strip()
+	return ret
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Make prediction object 		detection on input images")
+	print("-- Staring --")
+	parser = argparse.ArgumentParser(description="Make prediction object 		detection on input images")
 	requiredArgs = parser.add_argument_group("required argument")
 	requiredArgs.add_argument("--input", dest="input", type=str,help="path to image for prediciton",required = True)
 	requiredArgs.add_argument("--show", dest="show", type=bool,help="If show prediciton effect",required = True)
@@ -29,30 +30,37 @@ def main():
 	input = args.input
 
   	# Initialize engine.
-    engine = DetectionEngine("model/output/output_compiled.tflite")
-    labels = ReadLabelFile("model/output/labels.txt)
+	engine = DetectionEngine("model/output/output_compiled.tflite")
+	labels = ReadLabelFile("model/output/labels.txt")
 
   	# Open image.
-    img = Image.open(input)
-    draw = ImageDraw.Draw(img)
+	img = Image.open(input)
+	print(type(img))
+	draw = ImageDraw.Draw(img)
 
-  	# Run inference.	
-    ans = engine.DetectWithImage(img, threshold=0.05, keep_aspect_ratio=True,       relative_coord=False, top_k=10)
+  	# Run inference.
+	print("-- Forward propagation --")	
+	ans = engine.DetectWithImage(img, threshold=0.05, keep_aspect_ratio=True,       relative_coord=False, top_k=10)
 
 	# Display result.
 	img = cv2.imread(input)
-	colors = {"square":(0,255,0), "triangle":(255,0,0)}
-  	if ans:
-  		for obj in ans:
-      			print ('-----------------------------------------')
-      			if labels:
-        			print(labels[obj.label_id])
-      			print ('score = ', obj.score)
-      			box = obj.bounding_box.flatten().tolist()
-      			print ('box = ', box)
-			if arg.show == True:
-				cv2.rectangle(img, box[0], box[3], 					      color[labels[obj.label_id]], 3)
-	if arg.show = True:
-		cv2.imshow('prediction', img)
+	colors = {"Square":(0,255,0), "Triangle":(255,0,0)}
+	if ans:
+		for obj in ans:
+			print ('-----------------------------------------')
+			if labels:
+				print(labels[obj.label_id])
+			print ('score = ', obj.score)
+			box = obj.bounding_box.flatten().tolist()
+			print ('box = ', box)
+			if args.show == True:
+				cv2.rectangle(img,(int(box[0]),int(box[1])),(int(box[2]),int(box[3])),colors[labels[obj.label_id]],3)
+				cv2.putText(img, labels[obj.label_id], (int(box[0])+5,int(box[1])-5), cv2.FONT_HERSHEY_SIMPLEX, 0.37, (255,255,255), 1, cv2.LINE_AA)
+	if args.show == True:
+		cv2.imshow('image', img)
+		cv2.waitKey(0)
+		cv2.destroyAllWindows()
 
 
+if __name__ == '__main__':
+  main()
--- realTime_predicion_with_camera.py
+++ realTime_predicion_with_camera.py
@@ -1,17 +1,58 @@
-import numpy as np
+import argparse
+import platform
+import subprocess
+import time
+from edgetpu.detection.engine import DetectionEngine
+from PIL import Image
+from PIL import ImageDraw
 import cv2
 
 
 
-cap = cv2.VideoCapture(0)
 
-while(True):
-    ret, frame = cap.read(0)
-    cv2.rectangle(frame, (100,40), (200,150), (0,255,0), 3)
-    cv2.putText(frame, "class_name", (110,35), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xff == ord('q'):
-        break
+# Function to read labels from text files.
+def ReadLabelFile(file_path):
+	with open(file_path, 'r', encoding="utf-8") as f:
+		lines = f.readlines()
+	ret = {}
+	for line in lines:
+		pair = line.strip().split(maxsplit=1)
+		ret[int(pair[0])] = pair[1].strip()
+	return ret
+
+
+def main():
+	print("-- Staring --")
+
+  	# Initialize engine.
+	engine = DetectionEngine("model/output/output_compiled.tflite")
+	labels = ReadLabelFile("model/output/labels.txt")
+
+	print("-- Start recording --")
+	# Initialize camera
+	cap = cv2.VideoCapture(0)
+	colors = {"Square":(0,255,0), "Triangle":(255,0,0)}
+
+	while(True):
+		ret, frame = cap.read(0)
+
+  		# Run inference.
+		img = Image.fromarray(frame)	
+		ans = engine.DetectWithImage(img,keep_aspect_ratio=True,relative_coord=False)
+
+		# Display result.
+		if ans:
+			for obj in ans:
+				box = obj.bounding_box.flatten().tolist()
+				cv2.rectangle(frame,(int(box[0]),int(box[1])),(int(box[2]),int(box[3])),colors[labels[obj.label_id]],3)
+				cv2.putText(frame, labels[obj.label_id], (int(box[0])+5,int(box[1])-5), cv2.FONT_HERSHEY_SIMPLEX, 0.37, (255,255,255), 1, cv2.LINE_AA)
+		cv2.imshow('frame',frame)
+		if cv2.waitKey(1) & 0xff == ord('q'):
+			break
     
-cap.release()
-cv2.destroyAllWindows()
+	cap.release()
+	cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+  main()
