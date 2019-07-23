c52f2ab1779b6d25783498eb146be33b1e19dc1c
Maciej Sączewski <saczewski.maciej@outlook.com>
2019-07-23 17:44:14 +0200

    predic script

--- predict.py
+++ predict.py
@@ -11,39 +11,33 @@ import cv2
 
 # Function to read labels from text files.
 def ReadLabelFile(file_path):
-  with open(file_path, 'r', encoding="utf-8") as f:
-    lines = f.readlines()
-  ret = {}
-  for line in lines:
-    pair = line.strip().split(maxsplit=1)
-    ret[int(pair[0])] = pair[1].strip()
-  return ret
+    with open(file_path, 'r', encoding="utf-8") as f:
+        lines = f.readlines()
+    ret = {}
+    for line in lines:
+        pair = line.strip().split(maxsplit=1)
+        ret[int(pair[0])] = pair[1].strip()
+    return ret
 
 
 def main():
-
-	parser = argparse.ArgumentParser(description="Make prediction object 		detection on input images")
+    parser = argparse.ArgumentParser(description="Make prediction object 		detection on input images")
 	requiredArgs = parser.add_argument_group("required argument")
-	requiredArgs.add_argument("--input", dest="input", type=str,
-                   help="path to image for prediciton",
-                   required = True)
-	requiredArgs.add_argument("--show", dest="show", type=bool,
-		   help="If show prediciton effect",
-		   required = True)
+	requiredArgs.add_argument("--input", dest="input", type=str,help="path to image for prediciton",required = True)
+	requiredArgs.add_argument("--show", dest="show", type=bool,help="If show prediciton effect",required = True)
 	args = parser.parse_args()
 	input = args.input
 
   	# Initialize engine.
-  	engine = DetectionEngine("model/output/output_compiled.tflite")
-  	labels = ReadLabelFile("model/output/labels.txt)
+    engine = DetectionEngine("model/output/output_compiled.tflite")
+    labels = ReadLabelFile("model/output/labels.txt)
 
   	# Open image.
-  	img = Image.open(input)
-  	draw = ImageDraw.Draw(img)
+    img = Image.open(input)
+    draw = ImageDraw.Draw(img)
 
   	# Run inference.	
-  	ans = engine.DetectWithImage(img, threshold=0.05, keep_aspect_ratio=True,
-                               relative_coord=False, top_k=10)
+    ans = engine.DetectWithImage(img, threshold=0.05, keep_aspect_ratio=True,       relative_coord=False, top_k=10)
 
 	# Display result.
 	img = cv2.imread(input)
